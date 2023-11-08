35cc6f9d0a6ded9b99b67b46c3195cd65e3b7a4c
Marc Padrutt <marc.padrutt@icloud.com>
2023-11-08 16:16:39 +0100

    Removing files I mistakenly added

--- bar.py
+++ bar.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 from src.pipeline.pipeline import Pipeline
 from src.pipeline.utils import get_quickstart_config
-from example_step import ExampleStep
 from src.steps.steps import SiriusFingerprint, ShrinkMgf
 from src.steps.steps.mzmine3.mzmine3 import MZmine3
 
@@ -11,31 +10,27 @@ pipeline = Pipeline(*get_quickstart_config(output_path=Path(f"output")))
 
 pipeline.set_input([Path("testdata/1.mzML"), Path("testdata/2.mzML")])
 
-# We add the new step here, with the value required in "Setup"
-pipeline.add_step(ExampleStep, {"message": "Hello world"})
+pipeline.add_step(
+    MZmine3,
+    {
+        "mzmine3_path": Path(
+            "/Applications/MZmine.app/Contents/MacOS/MZmine", absolute=True
+        ),
+        "batchfile": Path("testdata/2.xml"),
+    },
+)
+
+pipeline.add_step(ShrinkMgf, {"compounds_per_file": 5})
+
+pipeline.add_step(
+    SiriusFingerprint,
+    {
+        "sirius_path": Path(
+            "/Applications/sirius.app/Contents/MacOS/sirius", absolute=True
+        ),
+        "set_max_mz": False,
+        "instrument": "orbitrap",
+    },
+)
 
 pipeline.run()
-
-
-# pipeline.add_step(
-#     MZmine3,
-#     {
-#         "mzmine3_path": Path(
-#             "/Applications/MZmine.app/Contents/MacOS/MZmine", absolute=True
-#         ),
-#         "batchfile": Path("testdata/2.xml"),
-#     },
-# )
-#
-# pipeline.add_step(ShrinkMgf, {"compounds_per_file": 5})
-#
-# pipeline.add_step(
-#     SiriusFingerprint,
-#     {
-#         "sirius_path": Path(
-#             "/Applications/sirius.app/Contents/MacOS/sirius", absolute=True
-#         ),
-#         "set_max_mz": False,
-#         "instrument": "orbitrap",
-#     },
-# )
