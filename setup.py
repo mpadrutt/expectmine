6dba63747cbc00eb2211ec0e568034563bc5f3c2
Marc Padrutt <marc.padrutt@icloud.com>
2024-01-13 19:04:04 +0100

    Trying to create a package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="expectmine",
+    version="0.0.1",
+    install_requires=[
+        "inflection",
+        "Jinja2",
+        "InquirerPy",
+        "requests",
+        "tqdm",
+        "python-dotenv",
+    ],
+)
