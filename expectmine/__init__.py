8d5d1db63e6e9d69dd8051aae9a42e3cce086048
Marc Padrutt <marc.padrutt@icloud.com>
2024-01-13 21:26:45 +0100

    Adding __init__.py

---
+++ expectmine/__init__.py
@@ -0,0 +1,13 @@
+"""
+Expectmine processing pipeline
+"""
+
+__version__ = "0.0.1"
+__author__ = "Marc Padrutt"
+
+import logger
+import storage
+import pipeline
+import utils
+import io
+import steps
