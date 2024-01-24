c30a75421ed4f4c570a0ce6885d843e80f274355
Marc Padrutt <marc.padrutt@icloud.com>
2024-01-24 20:26:25 +0100

    Trying click

---
+++ bin/cli.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+def main():
+    print("Hello from expectmine CLI!")
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
-    scripts=["bin/cli_script"],
+    scripts=['bin/cli.py'],
     install_requires=[
         "inflection",
         "Jinja2",
