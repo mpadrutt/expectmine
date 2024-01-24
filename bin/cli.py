0c334e70946a8b154a1b49ddf6e4ac45234a0ea0
Marc Padrutt <marc.padrutt@icloud.com>
2024-01-24 22:02:00 +0100

    Finally fixed??

--- bin/cli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-def main():
-    print("Hello from expectmine CLI!")
-
-
-if __name__ == "__main__":
-    main()
--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="expectmine",
-    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
-    scripts=['bin/cli.py'],
-    install_requires=[
-        "inflection",
-        "Jinja2",
-        "InquirerPy",
-        "requests",
-        "tqdm",
-        "python-dotenv",
-    ],
 )
