06c3e139fd62299d6a3aae3885105e3c71244912
Marc Padrutt <marc.padrutt@icloud.com>
2024-01-25 16:31:20 +0100

    Working on cli

--- expectmine/main.py
+++ expectmine/main.py
@@ -1,3 +1,19 @@
-def main():
-    print("Hello from expectmine CLI!")
+import argparse
 
+parser = argparse.ArgumentParser(
+    prog="ProgramName",
+    description="What the program does",
+    epilog="Text at the bottom of help",
+)
+
+parser.add_argument("--create", action="store_true", help="Call the create step flow.")
+
+
+def main():
+    args = parser.parse_args()
+
+    if args.create:
+        print("Create")
+        return
+
+    print("Normal")
