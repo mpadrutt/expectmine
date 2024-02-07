ad428d19923160ae0471c6ae61e5bbcb6c94ca7a
Marc Padrutt <marc.padrutt@icloud.com>
2024-02-07 15:21:18 +0100

    Cleanup project

--- examples/filter_step.py
+++ examples/filter_step.py
@@ -7,8 +7,20 @@ from expectmine.steps.steps import FilterMgf
 
 pipeline = Pipeline(*get_quickstart_config(output_path=Path("output")))
 
-pipeline.set_input([Path("sirius_output.mgf")])
+pipeline.set_input([Path("./examples/sirius_output.mgf")])
 
-pipeline.add_step(FilterMgf, {})
+pipeline.add_step(
+    FilterMgf,
+    {
+        "discard_pepmass": True,
+        "discard_filepath": Path("discard.txt"),
+        "error": 50000000,
+        "should_stop": False,
+        "filter_missing_ms": True,
+        "should_filter_filename": False,
+        "filter_filename": Path("file.txt"),
+    },
+)
+
 
 pipeline.run()
